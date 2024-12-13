0831a480932a32871b7bb20c81fca4ead3c85474
natassia-nn <natassianguyen@gmail.com>
2024-12-13 09:57:59 -0800

    made changes

--- Person.java
+++ Person.java
@@ -1,4 +1,48 @@
 public class Person
 {
+    private String first;
+    private String last;
+    private int age;
+    private int social;
+
+    public String getFirst()
+    {
+        return first;
+    }
+
+    public String getLast()
+    {
+        return last;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public int getSocial()
+    {
+        return social;
+    }
+    
+    public void setFirst(String firstName)
+    {
+        first = firstName;
+    }
+
+    public void setLast(String lastName)
+    {
+        last = lastName;
+    }
+
+    public void setAge(int iAge)
+    {
+        age = iAge;
+    }
+
+    public void setSocial(int SSN)
+    {
+        social = SSN;
+    }
 
 }
